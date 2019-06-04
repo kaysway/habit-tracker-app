f3a2ed253ec88188f0a578b10395e56a526e254c
Kayla Meiklejohn <kaysway90s@gmail.com>
2019-06-04 08:28:32 -0600

    added app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+//mock data
+var mock_log_posts = {
+ "logPosts": [
+     {
+        "id": "111111",
+        "text": "Ran two miles",
+        "publishedAt": 1470016976609
+     },
+     {
+         "id": "222222",
+         "text": "Drank 16oz of water",
+         "publishedAt": 1470016976609
+     },
+     {
+         "id": "333333",
+         "text": "Biked 16 miles",
+         "publishedAt": 1470016976609
+     }
+ ]   
+};
