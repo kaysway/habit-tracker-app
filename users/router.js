dfe30d62753a24a8610c71d9a56691de74c19a29
Kayla Meiklejohn <kaysway90s@gmail.com>
2019-06-17 16:42:12 -0600

    updated router file

--- users/router.js
+++ users/router.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const { User } = require('../models/user');
+const { User, Goal } = require('./models');
 
 const router = express.Router();
 
-router.post('/', jsonParser, (req, res) => {
+router.post('/user', jsonParser, (req, res) => {
     const requiredFields = ['username', 'password'];
     const missingField = requiredFields.find(
         field => !(field in req.body)
@@ -74,9 +74,40 @@ router.post('/', jsonParser, (req, res) => {
                 message: 'Server error while creating user'
             });
         });
-}
 
+    router.get('/goal', (req, res) => {
+        Goal
+            .findbyId(req.params.id)
+            .then(goal => {
+                return res.json(goal.serialize());
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).json({ error: 'Internal server error' });
+            });
+    })
 
+    router.put('/goal/:id', (req, res) => {
+        if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+            res.status(400).json({
+                error: 'Request path id and request body id values must match'
+            });
+        }
 
-module.exports = router;
+        const updated = {};
+        const updateableFields = ['goal', 'value'];
+        updateableFields.forEach(field => {
+            if (field in req.body) {
+                updated[field] = req.body[field];
+            }
+        });
+
+        Goal
+        .findByIdAndUpdate(req.params.id, { $set: updated }, {new: true })
+        .then(updatedGoal => res.status(204).end())
+        .catch(err => res.status(500).json({ message: 'Internal server error'}));
+    })
+};
+
+module.exports = { router };
 
